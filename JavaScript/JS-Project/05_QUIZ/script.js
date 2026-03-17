9b1c5204a499921a68f8b8cc8a57ed6c1d7e2d7c
Shiv Kant <shivkant639624@gmail.com>
2026-03-17 19:40:48 +0530

    Quiz App completed

--- JavaScript/JS-Project/05_QUIZ/script.js
+++ JavaScript/JS-Project/05_QUIZ/script.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       question: "Which planet is known as the Red Planet?",
       choices: ["Earth", "Mars", "Jupiter", "Saturn"],
+      answer: "Mars",
     },
     {
       question: "Who wrote the play 'Romeo and Juliet'?",
@@ -21,10 +22,12 @@ document.addEventListener("DOMContentLoaded", () => {
         "Leo Tolstoy",
         "Mark Twain",
       ],
+      answer: "William Shakespeare",
     },
     {
       question: "Which programming language runs in a web browser?",
       choices: ["Python", "Java", "C++", "JavaScript"],
+      answer: "JavaScript",
     },
     {
       question: "What is the largest ocean on Earth?",
@@ -34,15 +37,76 @@ document.addEventListener("DOMContentLoaded", () => {
         "Pacific Ocean",
         "Arctic Ocean",
       ],
+      answer: "Pacific Ocean",
     },
     {
       question: "Which country is famous for the Great Wall?",
       choices: ["Japan", "China", "India", "Thailand"],
+      answer: "China",
     },
   ];
 
   let currentQuestionIndex = 0;
   let score = 0;
+  let answered = false;
 
-  startBtn.addEventListener("click", () => {});
+  startBtn.addEventListener("click", () => {
+    startBtn.classList.add("hidden");
+    questionContainer.classList.remove("hidden");
+    currentQuestionIndex = 0;
+    score = 0;
+    showQuestion();
+  });
+
+  function showQuestion() {
+    answered = false;
+    const question = questions[currentQuestionIndex];
+    // console.log(question);
+    questionText.innerHTML = "";
+    questionText.textContent = question.question;
+
+    choicesList.innerHTML = "";
+    nextBtn.classList.add("hidden");
+    question.choices.forEach((option) => {
+      const li = document.createElement("li");
+      li.textContent = option;
+
+      li.addEventListener("click", () => {
+        if (answered) return;
+        answered = true; // lock after first click
+        if (option === question.answer) {
+          score++;
+        }
+        // console.log(score);
+        nextBtn.classList.remove("hidden");
+      });
+      choicesList.appendChild(li);
+    });
+  }
+
+  nextBtn.addEventListener("click", () => {
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < questions.length) {
+      showQuestion();
+    } else {
+      showScore();
+    }
+  });
+
+  function showScore() {
+    questionContainer.classList.add("hidden");
+    nextBtn.classList.add("hidden");
+    resultsContainer.classList.remove("hidden");
+    scoreDisplay.textContent = `${score} / ${questions.length}`;
+  }
+
+  restartBtn.addEventListener("click", () => {
+    resultsContainer.classList.add("hidden");
+    questionContainer.classList.remove("hidden");
+
+    currentQuestionIndex = 0;
+    score = 0;
+    showQuestion();
+  });
 });
