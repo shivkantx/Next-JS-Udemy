8721635c76fdf26e6dcb02e8e74abbbbdc9c7215
Shiv Kant <shivkant639624@gmail.com>
2026-04-06 10:40:39 +0530

    parallel route example - 2 with tab

---
+++ JavaScript/Next-JS/01-mini-project/app/dashboard-main/@tab1/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Tab1Page() {
+  return <div>Tab1Page</div>;
+}
+
+export default Tab1Page;
--- JavaScript/Next-JS/01-mini-project/app/dashboard-main/layout.js
+++ JavaScript/Next-JS/01-mini-project/app/dashboard-main/layout.js
@@ -1,8 +1,17 @@
-export default function DashboardLayout({ stats, feed }) {
+import Link from "next/link";
+
+export default function DashboardMainLayout({ tab1, tab2 }) {
   return (
-    <div style={{ display: "flex", gap: "20px", color: "gray-900" }}>
-      <div style={{ flex: 2, backgroundColor: "#f9fafb" }}>{stats}</div>
-      <div style={{ flex: 1, backgroundColor: "lightgray" }}>{feed}</div>
+    <div className="text-gray-900">
+      <nav style={{ marginBottom: "10px" }}>
+        <Link href="/dashboard-main">Tab 1</Link> |{" "}
+        <Link href="/dashboard-main">Tab 2</Link>
+      </nav>
+
+      <div>
+        {tab1}
+        {tab2}
+      </div>
     </div>
   );
 }
